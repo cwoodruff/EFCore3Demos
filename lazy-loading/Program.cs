89c6773e3b4d90669d0173fd2dd395e49b39eb5e
Chris Woodruff <cwoodruff@live.com>
2020-01-29 10:59:25 -0500

    Migrated demos to .NET Core 3.1

---
+++ lazy-loading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Performance.EFCore;
+
+namespace Demos
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new AdventureWorksContext())
+            {
+                var customers = db.Customers.ToList();
+
+                foreach (var customer in customers)
+                {
+                    if (customer.Store != null && customer.Territory != null)
+                        Console.WriteLine(customer.Store.Name + " located at " + customer.Territory.Name);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
