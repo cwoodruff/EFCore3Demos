506e0a7590399b7b09ef2215e76ebd0fd42c8a48
Chris Woodruff <cwoodruff@live.com>
2020-02-01 10:53:13 -0500

    added demo for FromSql

---
+++ from_sql/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using from_sql.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace from_sql
+{
+    class Program
+    {
+        static void Main()
+        {
+            using var db = new AdventureWorksContext();
+
+            var searchLastName = "Smith";
+
+            var people =
+                db.Person.FromSqlInterpolated(
+                    //$"SELECT [BusinessEntityID],[PersonType],[NameStyle],[Title],[FirstName],[MiddleName],[LastName],[Suffix],[EmailPromotion],[AdditionalContactInfo],[Demographics],[rowguid],[ModifiedDate] FROM [Person].[Person] WHERE LastName = {searchLastName}");
+                    $"SELECT * FROM [Person].[Person] WHERE LastName = {searchLastName}");
+            
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Found Person: {person.FirstName} {person.LastName}");
+            }
+        }
+    }
+}
